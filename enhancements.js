8e366e95bc586a3c55b23b91ced7402340cbdbd2
Thomas McGrew <tjmcgrew@gmail.com>
2009-03-13 15:02:58 -0400

    Set windows that are being drug to have the 'dragging' class so they can be styled with CSS

--- enhancements.js
+++ enhancements.js
@@ -13,6 +13,39 @@ MochaUI.extend({
 			if ( window_.options.closable )
 				this.closeWindow( $(window_.options.id) );
 		}.bind(this));
-	},
+	}
 
+});
+
+MochaUI.Window.implement({
+	attachDraggable: function(windowEl){
+		if (!this.options.draggable) return;
+		this.windowDrag = new Drag.Move(windowEl, {
+			handle: this.titleBarEl,
+			container: this.options.restrict == true ? $(this.options.container) : false,
+			grid: this.options.draggableGrid,
+			limit: this.options.draggableLimit,
+			snap: this.options.draggableSnap,
+			onStart: function() {
+				if (this.options.type != 'modal' && this.options.type != 'modal2'){ 
+					MochaUI.focusWindow(windowEl);
+					$('windowUnderlay').setStyle('display','block');
+					windowEl.addClass( 'dragging' );
+				}
+				if ( this.iframeEl )
+					this.iframeEl.setStyle('visibility', 'hidden');
+			}.bind(this),
+			onComplete: function() {
+				if (this.options.type != 'modal' && this.options.type != 'modal2') {
+					$('windowUnderlay').setStyle('display', 'none');
+					windowEl.removeClass( 'dragging' );
+				}
+				if ( this.iframeEl ){
+					this.iframeEl.setStyle('visibility', 'visible');
+				}
+				// Store new position in options.
+				this.saveValues();
+			}.bind(this)
+		});
+	}
 });
